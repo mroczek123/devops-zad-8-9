a3af2475073e23fd39cc272f6a3287b5594bfe07
Maciej Kulesza <mck.kulesza@gmail.com>
2026-05-16 09:51:56 +0200

    naprawione testy

--- backend/test/unit_tests/settings.test.js
+++ backend/test/unit_tests/settings.test.js
@@ -3,5 +3,5 @@ import assert from 'node:assert';
 import { Settings } from '../../src/settings.js';
 
 test("Settings is not undefined", (t) => {
-  assert.notStrictEqual(1, 1);
+  assert.notStrictEqual(Settings, undefined);
 });
