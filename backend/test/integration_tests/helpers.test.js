06e11928b961df65a932b2c8f5e273eecf7d85ff
Maciej Kulesza <mck.kulesza@gmail.com>
2026-05-16 08:47:07 +0200

    1

--- backend/test/integration_tests/helpers.test.js
+++ backend/test/integration_tests/helpers.test.js
@@ -4,6 +4,6 @@ import assert from 'node:assert';
 
 test("CountProducts", async (t) => {
   const result = await countProducts();
-  console.log({result});
-  assert.equal(await countProducts(), 3);
+  t.diagnostic({result});
+  assert.equal(result, 3);
 });
